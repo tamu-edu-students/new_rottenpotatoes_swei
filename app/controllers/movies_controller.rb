a63461580f487a3ed75eaf8d789dfdeacf122e19
danielswei <djs2002@gmail.com>
2024-12-31 14:54:28 +0800

    ruby controller for sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,8 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    @movies = Movie.order("#{params[:sort]} #{params[:direction]}")
   end
 
   # GET /movies/1 or /movies/1.json
